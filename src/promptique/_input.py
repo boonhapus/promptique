e047ce2e188d7559aca5e665d36ab597c6488eb6
boonhapus <me@boonhap.us>
2024-01-10 22:47:47 -0600

    clean up keyboard listener, expose async methods

--- src/promptique/_input.py
+++ src/promptique/_input.py
@@ -1,57 +1,47 @@
 from __future__ import annotations
 
-from typing import Callable, TypedDict
+from typing import Any, Callable, Optional
 import asyncio
 import collections
 import datetime as dt
 import inspect
 
 from prompt_toolkit.input import create_input
-from prompt_toolkit.key_binding import KeyPress
+from prompt_toolkit.key_binding import KeyPress  # noqa: TCH002
 from prompt_toolkit.keys import Keys
 import pydantic
 
 
-class HookSignature(TypedDict):
-    key: Keys
+class KeyPressContext(pydantic.BaseModel, arbitrary_types_allowed=True):
+    """Metadata about when a KeyPress happened."""
 
-Hook = Callable[HookSignature, None]
-
-
-class KeyContext(pydantic.BaseModel, arbitrary_types_allowed=True):
-    key: KeyPress
-    when: dt.datetime = pydantic.Field(default_factory=dt.datetime.now)
+    key_press: KeyPress
+    when: pydantic.AwareDatetime = pydantic.Field(default_factory=dt.datetime.utcnow)
 
     def __str__(self) -> str:
-        return f"<KeyContext key={self.key.key}, when='{self.when:%H:%M:%S.%f}'>"
+        return f"<KeyPressContext key={self.key_press.key}, when='{self.when:%H:%M:%S.%f}'>"
 
 
 class KeyboardListener:
     """Listen for keys from the keyboard."""
 
     def __init__(self):
-        self._background_done: asyncio.Event = None
-        self._background_task: asyncio.Task = None
-        self._key_hooks: dict[Keys, list[Hook]] = collections.defaultdict(list)
+        self._background_done: Optional[asyncio.Event] = None
+        self._background_task: Optional[asyncio.Task] = None
+        self._key_hooks: dict[str, list[Callable[[KeyPressContext], Any]]] = collections.defaultdict(list)
 
     async def _background_listener(self) -> None:
+        """Engage prompt_toolkit to listen for keys cross-platform."""
         self._input = tk_input = create_input()
 
         with tk_input.raw_mode():
-            with tk_input.attach(self._fire_callbacks):
+            with tk_input.attach(self._trigger_callbacks):
+                assert self._background_done is not None, "KeyboardListener has not yet been started"
                 await self._background_done.wait()
 
-    async def invoke(self, hook, *params):
-        parameter_count = len(inspect.signature(hook).parameters)
-        result = hook(*params[:parameter_count])
-
-        if inspect.isawaitable(result):
-            result = await result
-
-        return result
-
-    def _fire_callbacks(self) -> None:
-        active: set[asyncio.Task] = set()
+    def _trigger_callbacks(self) -> None:
+        """Trigger hooked callbacks on key press."""
+        active_tasks: set[asyncio.Task] = set()
 
         for key_press in self._input.read_keys():
             hooks = [
@@ -62,14 +52,24 @@ class KeyboardListener:
             if hooks is None:
                 continue
 
-            ctx = KeyContext(key=key_press)
+            ctx = KeyPressContext(key_press=key_press)
 
             for hook in hooks:
-                task = asyncio.create_task(self.invoke(hook, ctx))
-                task.add_done_callback(lambda t: active.remove(t))
-                active.add(task)
+                task = asyncio.create_task(self._invoke(hook, ctx))
+                task.add_done_callback(lambda t: active_tasks.discard(t))
+                active_tasks.add(task)
 
-    def add_binding(self, key: Keys, callback) -> None:
+    async def _invoke(self, hook: Callable[[KeyPressContext], Any], *params) -> Any:
+        """Invoke a keybound callback."""
+        parameter_count = len(inspect.signature(hook).parameters)
+        result = hook(*params[:parameter_count])
+
+        if inspect.isawaitable(result):
+            result = await result
+
+        return result
+
+    def add_binding(self, key: Keys, callback: Callable) -> None:
         """Add a callback to a key press."""
         self._key_hooks[key].append(callback)
 
@@ -77,8 +77,11 @@ class KeyboardListener:
         """Synchronous interface to starting a keyboard listener."""
         asyncio.run(self.start(wait=True))
 
-    async def start(self, wait: bool = False) -> None:
+    async def start(self, *, wait: bool = False, ignore_control_c: bool = False) -> None:
         """Start the KeyboardListener."""
+        if not ignore_control_c:
+            self.add_binding(Keys.ControlC, callback=self.stop)
+
         self._background_done = asyncio.Event()
         self._background_task = asyncio.create_task(self._background_listener())
 
@@ -87,11 +90,18 @@ class KeyboardListener:
 
     async def stop(self) -> None:
         """Stop the KeyboardListener."""
+        assert self._background_done is not None, "KeyboardListener has not yet been started"
         self._background_done.set()
 
 
 if __name__ == "__main__":
+    import logging
+
+    from rich.logging import RichHandler
+
+    logging.basicConfig(format="%(message)s", datefmt="[%X]", level=logging.INFO, handlers=[RichHandler()])
+    log = logging.getLogger(__name__)
+
     kb = KeyboardListener()
-    kb.add_binding(key=Keys.Any, callback=lambda ctx: print(ctx))
-    kb.add_binding(key=Keys.Escape, callback=kb.stop)
+    kb.add_binding(key=Keys.Any, callback=lambda ctx: log.info(ctx))
     kb.run()
