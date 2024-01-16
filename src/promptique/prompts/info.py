a0d9fc415f66f63fd9d38dd861dd479f90b1f2be
boonhapus <me@boonhap.us>
2024-01-16 11:53:09 -0600

    allow spinner to be placed as the marker or detail

--- src/promptique/prompts/info.py
+++ src/promptique/prompts/info.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
-from typing import Callable
+from typing import Callable, Literal
+import functools as ft
 import itertools as it
 import threading
 import time
@@ -27,22 +28,30 @@ class Spinner(BasePrompt):
     background: Callable
     icons: Iterable[str] = ("◒", "◐", "◓", "◑")
     rate: float = 3.0
+    location: Literal["MARKER", "DETAIL"] = "MARKER"
 
     def interactivity(self, live: Live) -> None:
         """Optionally give the User some time to read."""
         stepper = it.cycle(self.icons)
         last_step = time.perf_counter()
 
+        if self.location == "MARKER":
+            set_placement = ft.partial(setattr, self, "_marker")
+
+        if self.location == "DETAIL":
+            set_placement = ft.partial(setattr, self, "detail")
+
+        set_placement(next(stepper))
+
         thread = threading.Thread(target=self.background)
         thread.start()
-        self.detail = f"Doing some work..\n{next(stepper)}"
 
         while thread.is_alive():
             delta = time.perf_counter() - last_step
 
             if delta <= (1 / self.rate):
                 time.sleep((1 / self.rate) - delta)
-                self.detail = f"Doing some work..\n{next(stepper)}"
+                set_placement(next(stepper))
                 live.refresh()
                 last_step = time.perf_counter()
 
