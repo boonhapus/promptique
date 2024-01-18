0dc78a2ea310868308b6100917bc659a75f9493f
boonhapus <me@boonhap.us>
2024-01-18 10:17:37 -0800

    prototype promptique keys interface

--- src/promptique/keys.py
+++ src/promptique/keys.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from typing import Any, Union
+
 from prompt_toolkit.keys import Keys as _PromptToolkitKeys
+import pydantic
 
 # TODO:
 # Create a proper namesake that implements a similar protocol to prompt_toolkit.keys.Keys , so users of our API don't
@@ -14,24 +17,56 @@ from prompt_toolkit.keys import Keys as _PromptToolkitKeys
 # - Keys.phrase("match this", case_sensitive=True)
 #
 Keys = _PromptToolkitKeys
+_key_cache: dict[str, Key] = {}
 
 
-# class Keys(pydantic.BaseModel, arbitrary_types_allowed=True):
-#     """Represent known keys."""
-#
-#     @classmethod
-#     def from_ptk_keys(cls, keys: _PromptToolkitKeys) -> Keys:
-#         return cls()
-#
-#     @classmethod
-#     @ft.cache
-#     def new_representation(cls, data) -> Keys:
-#         return cls(...)
-#
-#     @classmethod
-#     def letter(cls, value: Any) -> Key:
-#         """Convert a single character into a Key"""
-#         if len(str(value)) > 1:
-#             raise ValueError(f"You must only provide single characters, got '{value}'")
-#         return cls.new_representation(key=str(value).encode())
-#
+def __getattr__(key_name: str) -> Key:
+    try:
+        key = _key_cache[key_name]
+    except KeyError:
+        raise AttributeError(f"AttributeError: module 'promptique.keys' has no attribute '{key_name}'") from None
+
+    return key
+
+
+class Key(pydantic.BaseModel, arbitrary_types_allowed=True):
+    """Represent known keys."""
+
+    name: str
+    data: Any
+    is_printable: bool = False
+
+    @pydantic.model_validator(mode="after")
+    def _cache_new_representation(self) -> Key:
+        _key_cache[self.name] = self
+        return self
+
+    @classmethod
+    def letter(cls, value: str, *, case_sensitive: bool = False) -> Key:
+        """Convert a single character into a Key"""
+        if len(value) > 1:
+            raise ValueError(f"You must provide only single characters, got '{value}'")
+
+        # Ensure we're caching both variants, future module level access will hit the cache
+        if not case_sensitive:
+            cls(name=value.upper(), data=value.upper())
+            cls(name=value.lower(), data=value.lower())
+
+        return _key_cache[value]
+
+    @classmethod
+    def number(cls, value: Union[str, int]) -> Key:
+        """Convert a single number into a Key"""
+        if not str(value).isdigit() or len(str(value)) > 1:
+            raise ValueError(f"You must provide only single numbers, got '{value}'")
+
+        return cls(name=f"number_{value}", data=value)
+
+
+# HOW TO PROPERLY DO THIS ONLY ONCE?
+if not _key_cache:
+    _key_cache["Space"] = Key(name="Space", data=" ", is_printable=True)
+
+    # See: https://github.com/prompt-toolkit/python-prompt-toolkit/blob/master/src/prompt_toolkit/keys.py
+    for key in _PromptToolkitKeys:
+        _key_cache[key.name] = Key(name=key.name, data=key.value, is_printable=False)
